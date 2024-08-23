d6e7f0ce44859437e88043014cf5d24aca1b91c7
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-23 16:46:40 +0100

    annotate

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,10 +6,11 @@ contains an asynchronous function
 
 import asyncio
 import bisect
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     asynchronously call the wait_random coroutine
     for n times
